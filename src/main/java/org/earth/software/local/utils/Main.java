7c761190debc71088d0a76ccfd028577bbf16507
David Pérez Serrada <dserrada@gmail.com>
2021-02-16 23:20:11 +0100

    Added first class

---
+++ src/main/java/org/earth/software/local/utils/Main.java
@@ -0,0 +1,10 @@
+package org.earth.software.local.utils;
+
+/**
+ * Initial launcher
+ */
+public class Main {
+    public static void main(String [] args) {
+        System.out.printf("Hello world");
+    }
+}
